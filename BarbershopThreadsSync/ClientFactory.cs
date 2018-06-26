68000fbdcd3a37e152a204c62f817c3f4d5d4fa1
blaze6950 <d.progect21@gmail.com>
2018-06-26 12:34:33 +0300

    Added ClientFactory class

---
+++ BarbershopThreadsSync/ClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarbershopThreadsSync
+{
+    class ClientFactory
+    {
+        private static int _count_s = 0;
+
+        public static int Count_s { get => _count_s; set => _count_s = value; }
+
+        public static Client CreateNewClient()
+        {
+            var newClient = new Client(_count_s.ToString());
+            _count_s++;
+            return newClient;
+        }
+    }
+}
